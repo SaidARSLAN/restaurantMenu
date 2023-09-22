7920ec0cd057f24233747582a5a335b89e070983
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-22 16:15:37 +0300

    started again

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import './App.css'
-import { Title,FilterNavbar,ProductList,Product,BuyBox,Footer } from './components'
-import InfoModal from './components/InfoModal'
-import { Provider } from './context/MainContext'
+
 function App() {
 
+
   return (
-    <Provider>
-    <main className='relative bg-white'>
-      <InfoModal />
-      <section className='pt-12 pb-4 px-4'>
-        <Title />
-      </section>
-      <section className='flex flex-col lg:flex-row'>
-      <section className='py-4 px-4'>
-        <FilterNavbar />
-      </section>
-      <section className='px-4'>
-        <ProductList />
-      </section>
-      </section>
-      <section className='py-12 px-4'>
-        <Footer />
-      </section>
-    </main>
-    </Provider>
+    <div></div>
   )
 }
 
--- src/components/BuyBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BuyBox = () => {
-  return (
-    <div>BuyBox</div>
-  )
-}
-
-export default BuyBox
