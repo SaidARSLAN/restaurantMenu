83504121f4afb225887ecc5759ad1003ffa2fe9c
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-23 12:49:21 +0300

    added gif

--- src/components/SendHamburger.jsx
+++ src/components/SendHamburger.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { cooking, sending } from '../contains'
+import { cooking } from '../contains'
 
 const SendHamburger = () => {
   return (
     <div className='flex flex-col w-full items-center lg:items-start justify-center'>
-        <img src={cooking} className='w-1/3 my-4' />
+        <img src={cooking} className='w-1/3 lg:w-2/3 my-4' />
         </div>
   )
 }
--- src/sections/Create.jsx
+++ src/sections/Create.jsx
@@ -24,8 +24,8 @@ const Create = () => {
         <img src={HamburgerNext} className='lg:w-[1440px]'/>
       </div>
       <div className='flex lg:items-start flex-col w-full lg:justify-center justify-center items-center  lg:px-24 lg:py-12'>
-      <h1 className='font-poppins text-xl lg:text-3xl   text-center lg:text-left'>
-        {step === 0 ? "Choose name and continue" : step === 1 ? "Choose your ingredients" : step === 2 ? "Confirm Everything" : step === 3 ? "Added to box": <></>}
+      <h1 className='font-poppins text-xl lg:text-5xl   text-center lg:text-left'>
+        {step === 0 ? "Choose Name" : step === 1 ? "Choose your ingredients" : step === 2 ? "Confirm Everything" : step === 3 ? "Added Box": <></>}
       </h1>
         {step === 0 ? <HamburgeName /> : step === 1 ? <HamburgerInfo /> : step === 2 ? <HamburgerConfirm />: step === 3 ? <SendHamburger />:<></>}
         <div className= {step === 3 ? 'hidden' : 'mt-2 w-2/3 flex justify-end'}>
