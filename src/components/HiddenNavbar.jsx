8376a2552ce231471f7ae5ba67800a0852076a06
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-24 19:03:36 +0300

    Added menu functions

--- src/components/HiddenNavbar.jsx
+++ src/components/HiddenNavbar.jsx
@@ -20,9 +20,9 @@ const HiddenNavbar = () => {
         <div className='flex flex-col space-y-12 items-center justify-center py-2'>
             <a href="/" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Home</a>
             <a href="#create" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Create</a>
-            <a href="" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Menu</a>
-            <a href="" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>About</a>
-            <a href="" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Comments</a>
+            <a href="#menu" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Menu</a>
+            <a href="#about" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>About</a>
+            <a href="#testimontials" className='text-2xl font-poppins text-black hover:bg-black hover:text-white duration-300 px-4 py-2'>Testimontials</a>
             <div className='flex justify-center space-x-6 items-center'>
                 <FontAwesomeIcon icon={faShoppingBag} className='text-2xl cursor-pointer'></FontAwesomeIcon>
                 <button className='text-lg font-poppins uppercase tracking-wide hover:bg-black hover:text-white duration-300 px-4 py-2'>Sign In</button>
--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const MenuCard = ({item}) => {
   return (
-    <div className='border-2 flex w-full  justify-between items-center px-4 shadow-lg py-2 lg:px-8 lg:py-8 lg:h-[480px]'>
+    <div className='border-2 flex w-full  justify-center lg:justify-between items-center px-4 shadow-lg py-2 lg:px-8 lg:py-8 lg:h-[480px]'>
         <div className='text-left flex flex-col'>
-        <img src={item.img} className='my-2'/>
+        <img src={item.img} className='my-2 w-[400px] h-[240px]'/>
         <div className='flex w-full justify-between items-center'>
         <div>
         <h1 className='text-xl font-poppins'>{item.name}</h1>
@@ -14,7 +14,7 @@ const MenuCard = ({item}) => {
         <h1 className='font-mulish text-sm'>{item.ingredients.map((ing,idx) => {return <li key={idx}>{ing}</li>})}</h1>
         </div>
         <button className='bg-yellow-400 flex w-[100px] items-center justify-center my-2 space-x-2 py-4 px-2 hover:bg-black hover:text-yellow-400 duration-300'>
-            <span className='text-lg font-poppins'><FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon></span><span className='text-lg font-poppins'>{item.price}</span>
+            <span className='text-lg font-poppins'><FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon></span><span className='text-lg font-poppins'>{item.price}$</span>
         </button>
         </div>
         </div>
